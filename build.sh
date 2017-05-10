817ee2e5e0160078e05a802b5701ffdcc3c17835
JSurf <jsurf@gmx.de>
2017-05-10 15:07:21 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
- #!/bin/sh
- apt-get update
- apt-get -y install git
- git clone https://git.zerfleddert.de/git/hmcfgusb
- cd hmcfgusb
- mk-build-deps -i
- dpkg-buildpackage -b -rfakeroot -us -uc
+#!/bin/sh
+apt-get update
+apt-get -y install git
+git clone https://git.zerfleddert.de/git/hmcfgusb
+cd hmcfgusb
+mk-build-deps -i
+dpkg-buildpackage -b -rfakeroot -us -uc
