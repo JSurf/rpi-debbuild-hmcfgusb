98f1945e27a64bc71e356becb27bad800b2d4c8a
JSurf <jsurf@gmx.de>
2017-05-10 13:18:54 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+ #!/bin/sh
+ cd /build
+ apt-get update
+ apt-get -y install git
+ git clone https://git.zerfleddert.de/git/hmcfgusb
+ cd hmcfgusb
+ mk-build-deps -i
+ dpkg-buildpackage -b -rfakeroot -us -uc
